c8ea9b6cf96865cb65abbad2c7e70a2e2e9dd294
Adrien Gras <72006541+AdrienGras@users.noreply.github.com>
2023-12-01 21:01:57 +0100

    day 1 proper

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, collections::HashMap};
 
 const FILEPATH: &str = "./input.txt";
 const TEST_FILE_PART1: &str = "./input_ex_1.txt";
@@ -98,6 +98,67 @@ fn part_2(file_path: &str) -> i32 {
     sum
 }
 
+fn generate_digit_map() -> HashMap<i32, String> {
+    let mut digit_map: HashMap<i32, String> = HashMap::new();
+
+    digit_map.insert(1, String::from("one"));
+    digit_map.insert(2, String::from("two"));
+    digit_map.insert(3, String::from("three"));
+    digit_map.insert(4, String::from("four"));
+    digit_map.insert(5, String::from("five"));
+    digit_map.insert(6, String::from("six"));
+    digit_map.insert(7, String::from("seven"));
+    digit_map.insert(8, String::from("eight"));
+    digit_map.insert(9, String::from("nine"));
+
+    digit_map
+}
+
+fn part_2_proper(file: &str) -> i32 {
+    let lines = read_to_string(file).unwrap();
+
+    let digit_map = generate_digit_map();
+
+    let sum = lines
+    .lines()
+    .collect::<Vec<&str>>()
+    .iter()
+    .fold(0, |acc, line| {
+        let line = line.to_string();
+
+        let line_values = (0..line.len()).filter_map(|index| {
+            let temp_line = &line[index..];
+
+            let first_char: char = temp_line.chars().next().unwrap();
+
+            if first_char.is_ascii_digit() {
+                Some(first_char.to_digit(10).unwrap() as i32)
+            } else {
+                for (digit, word) in digit_map.iter() {
+                    if temp_line.starts_with(word) {
+                        return Some(*digit);
+                    }
+                }
+
+                None
+            }
+        })
+        .collect::<Vec<i32>>();
+
+        if line_values.is_empty() {
+            return acc;
+        }
+
+        let first_digit = line_values[0];
+        let last_digit = line_values[line_values.len() - 1];
+
+        let number = format!("{}{}", first_digit, last_digit).parse::<i32>().unwrap();
+        acc + number
+    });
+
+    sum
+}
+
 fn main() {
     println!("=== Advent of Code 2023 - Day 1 ===\n");
 
@@ -133,5 +194,22 @@ fn main() {
 
     println!("PART2: Input file");
     let result = part_2(FILEPATH);
+    println!("Result: {}", result);
+
+    println!("\n--- Part 2 proper way ---");
+
+    println!("PART2: Test file");
+    let result = part_2_proper(TEST_FILE_PART2);
+    let expected = 281;
+    println!("Test result: {} (expected: {})", result, expected);
+
+    if result == expected {
+        println!("Test successful!");
+    } else {
+        panic!("Test failed!");
+    }
+
+    println!("PART2: Input file");
+    let result = part_2_proper(FILEPATH);
     println!("Result: {}", result);
 }
